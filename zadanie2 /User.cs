55740f7d9cdfeced49c27b557ca1642319f9a196
sshikai <neodext56@gmail.com>
2025-12-09 22:04:32 +0300

    Create User.cs

---
+++ zadanie2 /User.cs
@@ -0,0 +1,48 @@
+using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace AdoSqlite.DAL
+{
+    public class User : IBindableComponent
+    {
+        //Members of IBindableComponent
+        ISite iSite;
+        ControlBindingsCollection dataBindings;
+        BindingContext bindingContext = new BindingContext();
+
+        public User()
+        {
+            dataBindings = new ControlBindingsCollection(this);
+        }
+        [Browsable(false)]
+        public event EventHandler Disposed;
+        [Browsable(false)]
+        public void Dispose()
+        {
+            //your code for disposing
+        }
+        [Browsable(false)]
+        public BindingContext BindingContext
+        {
+            get { return bindingContext; }
+            set { bindingContext = value; }
+        }
+        [Browsable(false)]
+        public ControlBindingsCollection DataBindings
+        {
+            get { return dataBindings; }
+        }
+        [Browsable(false)]
+        public ISite Site
+        {
+            get { return iSite; }
+            set { iSite = value; }
+        }
+
+        public int Id { get;  set; }
+        public string UserName { get;  set; }
+        public string Name { get;  set; }
+        public DateTime Date { get;  set; }
+    }
+}
